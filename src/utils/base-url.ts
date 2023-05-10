cd69c948cb98509a00daf82d4d278a6e61624a55
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-05-10 14:27:09 +0400

    feat: add dynamic base url

--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -1,3 +1,7 @@
-const BASE_URL = 'http://localhost:3001/';
+const {NODE_ENV} = process.env;
+
+const BASE_URL = NODE_ENV === 'production' 
+  ? 'https://api.mesto.nikolaykrish.nomoredomains.icu/' 
+  : 'http://localhost:3001/';
 
 export default BASE_URL;
