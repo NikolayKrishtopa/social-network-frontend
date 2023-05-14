6417805cf0c206d642e741471f63052ca2f406e2
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-05-14 15:50:46 +0400

    fix: redo grid layout for users page

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import s from './UserCard.module.scss';
 import { UserCardsProps } from './UserCard.props';
 import { useAppSelector, useAppDispatch } from '../../hooks/reduxHooks';
-import { addUserToFriends, removeUserFromFriends } from '../../store/slices/usersSlice';
+import {
+  addUserToFriends,
+  removeUserFromFriends,
+} from '../../store/slices/usersSlice';
 import { Link } from 'react-router-dom';
 
-export default function UserCard(props: UserCardsProps){
-  const {name, avatar, _id} = props.user;
-  const {currentUser} = useAppSelector(state => state.auth);
+export default function UserCard(props: UserCardsProps) {
+  const { name, avatar, _id } = props.user;
+  const { currentUser } = useAppSelector((state) => state.auth);
   const isFriend = currentUser?.friends.includes(_id);
 
   const dispatch = useAppDispatch();
@@ -20,19 +23,20 @@ export default function UserCard(props: UserCardsProps){
     dispatch(removeUserFromFriends(_id));
   };
 
-  return(
+  return (
     <li className={s.card}>
-      <Link to={`/${_id}`}>
+      <Link to={`/${_id}`} className={s.link}>
         <div className={s.imgContainer}>
           <img src={avatar} alt={name} className={s.img} />
         </div>
       </Link>
       <h3 className={s.title}>{name}</h3>
-      <button 
+      <button
         className={isFriend ? s.btnFriend : s.btnDefault}
         onClick={isFriend ? removeFriend : addFriend}
       >
         {isFriend ? 'Удалить из друзей' : 'Добавить в друзья'}
       </button>
     </li>
-  );}
+  );
+}
